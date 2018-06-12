6c7eadd1552ebe0e165a8237ce62bc7b1ef475b6
Moul <moul@moul.re>
2018-06-12 23:28:21 +0200

    [enh] #95: split f() to display the license.

--- src/license.py
+++ src/license.py
@@ -2,9 +2,16 @@ import webbrowser
 from pydoc import pager
 from sys import exit
 
+
 def license_approval(currency):
     if currency != "g1":
         return
+    display_license()
+    if (input("Do you approve Ğ1 license [yes/no]? ") != "yes"):
+        exit(1)
+
+
+def display_license():
     language = input("In which language would you like to display Ğ1 license [en/fr]? ")
     if (language == "en"):
         if not webbrowser.open("https://duniter.org/en/get-g1/"):
@@ -12,6 +19,3 @@ def license_approval(currency):
     else:
         if not webbrowser.open("https://duniter.org/fr/wiki/licence-g1/"):
             pager(open("licence-G1/license/license_g1-fr-FR.rst").read())
-
-    if (input("Do you approve Ğ1 license [yes/no]? ") != "yes"):
-        exit(1)
