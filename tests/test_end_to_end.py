44fe83d259cee0e3c587859aef11333932f2d447
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-07-11 14:12:36 +0200

    [enh] add test for wot command

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -2,7 +2,18 @@ from subprocess import check_output
 
 
 def test_info():
-    """tests './silkaj info' returns a number of members"""
+    """tests 'silkaj info' returns a number of members"""
 
     output = check_output(["silkaj", "info"])
     assert "Number of members" in output.decode()
+
+
+def test_wot():
+    """tests 'silkaj wot' returns a number of members"""
+
+    output = check_output(["silkaj", "wot", "moul"]).decode()
+    assert "moul (GfKER…) from block #0-E3B0C44298FC1…" in output
+    assert "received_expire" in output
+    assert "received" in output
+    assert "sent" in output
+    assert "sent_expire" in output
