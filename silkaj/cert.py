1b7ba4330c9dc45181c5ca78902791878fd71388
Moul <moul@moul.re>
2018-07-13 10:27:29 +0200

    [mod] assign variable to avoid many researches in dicts.

--- silkaj/cert.py
+++ silkaj/cert.py
@@ -13,6 +13,7 @@ from silkaj.wot import is_member,\
 def send_certification(ep, cli_args):
     current_blk = get_current_block(ep)
     id_to_certify = get_informations_for_identity(ep, cli_args.subsubcmd)
+    main_id_to_certify = id_to_certify["uids"][0]
 
     # Check that the id is present on the network
     if id_to_certify["pubkey"] is NO_MATCHING_ID:
@@ -35,14 +36,14 @@ def send_certification(ep, cli_args):
         message_exit("You can’t certify yourself!")
 
     # Check if this certification is already present on the network
-    for certifier in id_to_certify["uids"][0]["others"]:
+    for certifier in main_id_to_certify["others"]:
         if certifier["pubkey"] == issuer_pubkey:
             message_exit("Identity already certified by " + issuer_id)
 
     # Certification confirmation
-    if not certification_confirmation(issuer_id, issuer_pubkey, id_to_certify):
+    if not certification_confirmation(issuer_id, issuer_pubkey, id_to_certify, main_id_to_certify):
         return
-    cert_doc = generate_certification_document(current_blk, issuer_pubkey, id_to_certify)
+    cert_doc = generate_certification_document(current_blk, issuer_pubkey, id_to_certify, main_id_to_certify)
     cert_doc += sign_document_from_seed(cert_doc, seed) + "\n"
 
     # Send certification document
@@ -50,23 +51,23 @@ def send_certification(ep, cli_args):
     print("Certification successfully sent.")
 
 
-def certification_confirmation(issuer_id, issuer_pubkey, id_to_certify):
+def certification_confirmation(issuer_id, issuer_pubkey, id_to_certify, main_id_to_certify):
     cert = list()
     cert.append(["Cert", "From", "–>", "To"])
-    cert.append(["ID", issuer_id, "–>", id_to_certify["uids"][0]["uid"]])
+    cert.append(["ID", issuer_id, "–>", main_id_to_certify["uid"]])
     cert.append(["Pubkey", issuer_pubkey, "–>", id_to_certify["pubkey"]])
     if input(tabulate(cert, tablefmt="fancy_grid") +
        "\nDo you confirm sending this certification? [yes/no]: ") == "yes":
         return True
 
 
-def generate_certification_document(current_blk, issuer_pubkey, id_to_certify):
+def generate_certification_document(current_blk, issuer_pubkey, id_to_certify, main_id_to_certify):
     return "Version: 10\n\
 Type: Certification\n\
 Currency: " + current_blk["currency"] + "\n\
 Issuer: " + issuer_pubkey + "\n\
 IdtyIssuer: " + id_to_certify["pubkey"] + "\n\
-IdtyUniqueID: " + id_to_certify["uids"][0]["uid"] + "\n\
-IdtyTimestamp: " + id_to_certify["uids"][0]["meta"]["timestamp"] + "\n\
-IdtySignature: " + id_to_certify["uids"][0]["self"] + "\n\
+IdtyUniqueID: " + main_id_to_certify["uid"] + "\n\
+IdtyTimestamp: " + main_id_to_certify["meta"]["timestamp"] + "\n\
+IdtySignature: " + main_id_to_certify["self"] + "\n\
 CertTimestamp: " + str(current_blk["number"]) + "-" + current_blk["hash"] + "\n"
