9f973a623de9e381d01bd6702d898dcab18affb0
Moul <moul@moul.re>
2019-01-05 23:23:21 +0100

    [enh] #166: add bash and zsh completion scripts

---
+++ shell_completion/silkaj-bash-complete.sh
@@ -0,0 +1,21 @@
+_silkaj_completion() {
+    local IFS=$'
+'
+    COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   _SILKAJ_COMPLETE=complete $1 ) )
+    return 0
+}
+
+_silkaj_completionetup() {
+    local COMPLETION_OPTIONS=""
+    local BASH_VERSION_ARR=(${BASH_VERSION//./ })
+    # Only BASH version 4.4 and later have the nosort option.
+    if [ ${BASH_VERSION_ARR[0]} -gt 4 ] || ([ ${BASH_VERSION_ARR[0]} -eq 4 ] && [ ${BASH_VERSION_ARR[1]} -ge 4 ]); then
+        COMPLETION_OPTIONS="-o nosort"
+    fi
+
+    complete $COMPLETION_OPTIONS -F _silkaj_completion silkaj
+}
+
+_silkaj_completionetup;
