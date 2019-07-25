8b3f9ec3679a2fb6ae3d7730344b9bba1eb23a04
Moul <moul@moul.re>
2019-07-25 21:12:35 +0200

    v0.7.3

--- silkaj/constants.py
+++ silkaj/constants.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License
 along with Silkaj. If not, see <https://www.gnu.org/licenses/>.
 """
 
-SILKAJ_VERSION = "0.7.2"
+SILKAJ_VERSION = "0.7.3"
 G1_SYMBOL = "Ğ1"
 GTEST_SYMBOL = "ĞTest"
 G1_DEFAULT_ENDPOINT = "g1.duniter.org", "443"
