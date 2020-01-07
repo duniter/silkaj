895dad1e4265c17486f6bb0a59eed34f004fe82f
Moul <moul@moul.re>
2020-01-07 20:31:54 +0200

    [mod] #252: Create helper to update the copyright year

---
+++ update_copyright_year.sh
@@ -0,0 +1,7 @@
+#!/bin/fish
+
+# Script to update the copyright year in header files
+
+set NEW_YEAR (date +"%Y")
+set OLD_YEAR (math $NEW_YEAR - 1)
+sed -i "s/Copyright  2016-$OLD_YEAR M/Copyright  2016-$NEW_YEAR M/g" bin/silkaj silkaj/*.py tests/*.py
