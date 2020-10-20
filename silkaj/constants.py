7d67a826130b5e665c29c378f94068ab2f1629bf
Moul <moul@moul.re>
2020-10-20 18:05:32 +0200

    [mod] #299: Start 0.9.0dev development cycle

--- silkaj/constants.py
+++ silkaj/constants.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License
 along with Silkaj. If not, see <https://www.gnu.org/licenses/>.
 """
 
-SILKAJ_VERSION = "0.8.0"
+SILKAJ_VERSION = "0.9.0dev"
 G1_SYMBOL = "Ğ1"
 GTEST_SYMBOL = "ĞTest"
 G1_DEFAULT_ENDPOINT = "g1.duniter.org", "443"
