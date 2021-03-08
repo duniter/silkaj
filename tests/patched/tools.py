f7312242f6905555f8762792f711255961f118ed
matograine <tom.ngr@zaclys.net>
2021-03-08 11:44:00 +0100

    [mod] add copyright header to tests/patched files

--- tests/patched/tools.py
+++ tests/patched/tools.py
@@ -1,5 +1,21 @@
-from silkaj.constants import G1_SYMBOL
+"""
+Copyright  2016-2021 Maël Azimi <m.a@moul.re>
 
+Silkaj is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Silkaj is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Silkaj. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from silkaj.constants import G1_SYMBOL
 
 # mock CurrencySymbol().symbol
 async def patched_currency_symbol(self):
